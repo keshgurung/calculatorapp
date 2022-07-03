44b2d770d813511b118ebceee932ca4d55f45e3f
keshgurung <k_grg@hotmail.com>
2022-07-03 20:45:18 +0100

    basic calculation

--- calc/calc/ViewController.swift
+++ calc/calc/ViewController.swift
@@ -11,7 +11,7 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var calculatorResults: UILabel!
     
-    var calculatorRun: String = ""
+    var calcValue: String = ""
    
     
     override func viewDidLoad() {
@@ -21,83 +21,114 @@ class ViewController: UIViewController {
     }
 
     func clearAll(){
-        calculatorRun = ""
-        calculatorRun.text = ""
-        calculatorResult.text=""
+        calcValue = ""
+        calculatorResults.text = ""
     }
     
     @IBAction func allClearButton(_ sender: Any) {
         clearAll()
     }
     
+    @IBAction func equalsButton(_ sender: Any) {
+       let expression = NSExpression(format: calcValue)
+        
+       let result = expression.expressionValue(with: nil, context: nil) as! Double
+       let resultString = formatResult(result: result)
+        calculatorResults.text = resultString
+        
+        
+    }
+    
+    func formatResult(result: Double) -> String {
+        if (result.truncatingRemainder(dividingBy: 1) == 0){
+            return String(format: "%.0f", result)
+        }
+        else{
+            return String(format: "%.2f", result)
+        }
+    }
+    
+    func calcFunctionValue(value: String){
+        calcValue = calcValue + value
+        calculatorResults.text = calcValue
+    }
+    
     
     @IBAction func divideButton(_ sender: Any) {
+        calcFunctionValue(value: "/")
     }
     
     
     @IBAction func multiplyButton(_ sender: Any) {
+        calcFunctionValue(value: "*")
     }
     
     @IBAction func subtractButton(_ sender: Any) {
+        calcFunctionValue(value: "-")
     }
     
     
-    func calcAdd(value: String){
-        calculatorRun = calculatorRun + value
-        calculatorRun.te
-    }
     @IBAction func addButton(_ sender: Any) {
+        calcFunctionValue(value: "+")
         
     }
     
     
     
-    @IBAction func equalsButton(_ sender: Any) {
-    }
-    
-    
     @IBAction func dotButton(_ sender: Any) {
+        calcFunctionValue(value: ".")
     }
     
     
     @IBAction func zeroButton(_ sender: Any) {
+        calcFunctionValue(value: "0")
     }
     
     
     
     
     @IBAction func oneButton(_ sender: Any) {
+        calcFunctionValue(value: "1")
     }
     
     
     @IBAction func twoButton(_ sender: Any) {
+        calcFunctionValue(value: "2")
     }
     
     
     @IBAction func threeButton(_ sender: Any) {
+        calcFunctionValue(value: "3")
     }
+    
     
     
     
     @IBAction func fourButton(_ sender: Any) {
+        calcFunctionValue(value: "4")
     }
     
     
     @IBAction func fiveButton(_ sender: Any) {
+        calcFunctionValue(value: "5")
     }
     
     
     @IBAction func sixButton(_ sender: Any) {
+        calcFunctionValue(value: "6")
     }
     
     @IBAction func sevenButton(_ sender: Any) {
+        calcFunctionValue(value: "7")
     }
     
     
     @IBAction func eightButton(_ sender: Any) {
+        calcFunctionValue(value: "8")
     }
     
     @IBAction func nineButton(_ sender: Any) {
+        calcFunctionValue(value: "9")
     }
     
 }
